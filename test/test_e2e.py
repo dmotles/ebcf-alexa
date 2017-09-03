0e4e8c9a5ddc01d6226face2526fba029298514f
Daniel Motles <seltom.dan@gmail.com>
2017-09-03 12:55:57 -0700

    New e2e testing file...
    This file will conduct integration tests as opposed to targeted unit
    tests. Updating it so it actually checks code and asserts on stuff

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -1,14 +1,70 @@
 import pytest
-from unittest.mock import NonCallableMagicMock, Mock, patch, mock_open
-from _ebcf_alexa import wods
+from unittest.mock import NonCallableMagicMock, patch, mock_open, Mock
+from _ebcf_alexa import wods, env
 from ebcf_alexa import lambda_handler
+from datetime import datetime
 
-EBCF_WEBSITE_JSON = r"""
-{"links":{"self":"http://www.elliottbaycrossfit.com/api/v1/wods?filter%5Bsimple%5D%5Bdate%5D=2017-06-01T00%3A00%3A00.000Z&filter%5Bsimple%5D%5Benabled%5D=True"},"data":[{"id":"591f33008961150004cbb9a0","type":"wods","attributes":{"enabled":true,"title":null,"date":"2017-06-02T00:00:00.000Z","publishDate":"2017-06-02T04:00:00.000Z","image":"http://ebcf.s3.amazonaws.com/6-1.jpg","strength":"Hang Power Snatch \n4x3\nDo not drop bar, work at moderate weight, focus on barbell cycling","conditioning":"30 Snatches 115#/95#\n30 Clean & Jerks 115#/95#\n30 Muscle Ups","description":null,"videoId":null},"links":{"self":"http://localhost:4500/api/v1/wods/591f33008961150004cbb9a0"},"relationships":{"tags":{"data":[{"type":"tags","id":"56e61406e17eab8d035f2a1e"},{"type":"tags","id":"56e61406e17eab8d035f2a74"},{"type":"tags","id":"56e61406e17eab8d035f2a4a"},{"type":"tags","id":"572e83adad8b7003006f9ede"},{"type":"tags","id":"591f32f88961150004cbb99f"}],"links":{"self":"http://localhost:4500/api/v1/wods/relationships/tags"}}}}]}
+
+def patch_urlopen(response_str: str) -> Mock:
+    """patch the API call for URLopen with a given response"""
+    return patch.object(wods, 'urlopen', mock_open(read_data=response_str))
+
+
+def assert_valid_response(resp: dict) -> None:
+    assert resp['version'] == '1.0'
+    assert 'response' in resp
+    resp_inner = resp['response']
+    if 'outputSpeech' in resp_inner:
+        output_speech = resp_inner['outputSpeech']
+        assert output_speech['type'] in ('PlainText', 'SSML')
+        if output_speech['type'] == 'PlainText':
+            assert 'text' in output_speech
+        else:
+            assert 'ssml' in output_speech
+    assert 'shouldEndSession' in resp_inner
+
+
+OPEN_SKILL = {'version': '1.0',
+              'session': {'new': True, 'sessionId': 'amzn1.echo-api.session.4f9873cb-18e1-48e7-b078-180aba73e6b3',
+                          'application': {'applicationId': 'amzn1.ask.skill.d6f2f7c4-7689-410d-9c35-8f8baae37969'},
+                          'user': {
+                              'userId': 'amzn1.ask.account.XXXXX'}},
+              'context': {'AudioPlayer': {'playerActivity': 'PLAYING'}, 'Display': {}, 'System': {
+                  'application': {'applicationId': 'amzn1.ask.skill.d6f2f7c4-7689-410d-9c35-8f8baae37969'}, 'user': {
+                      'userId': 'amzn1.ask.account.XXXXX'},
+                  'device': {
+                      'deviceId': 'amzn1.ask.device.XXXX',
+                      'supportedInterfaces': {'AudioPlayer': {},
+                                              'Display': {'templateVersion': '1.0', 'markupVersion': '1.0'},
+                                              'VideoApp': {}}}, 'apiEndpoint': 'https://api.amazonalexa.com'}},
+              'request': {'type': 'LaunchRequest',
+                          'requestId': 'amzn1.echo-api.request.1552ecd8-907e-4308-8fd6-4710100120be',
+                          'timestamp': '2017-09-03T18:34:11Z', 'locale': 'en-US'}}
+
+
+EBCF_RESPONSE_WOD_20170901 = r"""
+{"meta":{},"links":{"self":"http://www.elliottbaycrossfit.com/api/v1/wods?filter%5Bsimple%5D%5Bdate%5D=2017-09-01T00%3A00%3A00.000Z&filter%5Bsimple%5D%5Benabled%5D=true"},"data":[{"id":"599cb1c179707c0004b994ed","type":"wods","attributes":{"enabled":true,"title":null,"date":"2017-09-01T00:00:00.000Z","publishDate":"2017-09-01T04:00:00.000Z","image":"http://ebcf.s3.amazonaws.com/IMG_1910.JPG","strength":"HAPPY BIRTHDAY JACOB!!!!\nFront Squat \n3x2 @ 80% of 1RM","conditioning":"2 Minutes\n15 Push Ups, 50 Double Unders, Max Reps OH Squats 95#/75#\nRest 1 Minutes\n2 Minutes\n15 Push Ups, 50 Double Unders, Max Reps OH Squats 95#/75#\nRest 1 Minutes\n2 Minutes\n15 Push Ups, 50 Double Unders, Max Reps OH Squats 95#/75#\nRest 1 Minutes\n3 Minutes\n15 Push Ups, 50 Double Unders, Max Reps OH Squats 95#/75#\n\nWorkout is complete when you complete 75 OH Squats, if you don't complete 75 OH Squats within the given intervals, your score is total number of squats","description":null,"videoId":null},"links":{"self":"http://localhost:4500/api/v1/wods/599cb1c179707c0004b994ed"},"relationships":{"tags":{"data":[{"type":"tags","id":"56e61406e17eab8d035f2a42"},{"type":"tags","id":"59a49db56ca7ef0004dd7e2c"}],"links":{"self":"http://localhost:4500/api/v1/wods/relationships/tags"}}}}]}
 """.strip()
 
+EBCF_RESPONSE_WOD_20170901_SSML = '''
+<speak><p>The workout for today, Friday September 1, 2017</p><p>Announcement:<s>HAPPY BIRTHDAY JACOB!!!!</s></p><p>Strength Section:</p><s>Front Squat</s><s>3 sets of 2 @ 80% of 1RM</s><p>Conditioning:</p><s>2 Minutes</s><s>15 Push Ups, 50 Double Unders, Max Reps <sub alias="overhead">OH</sub> Squats <prosody rate="fast">95<sub alias="pounds">#</sub> male, 75<sub alias="pounds">#</sub> female</prosody></s><s>Rest 1 Minutes</s><s>2 Minutes</s><s>15 Push Ups, 50 Double Unders, Max Reps <sub alias="overhead">OH</sub> Squats <prosody rate="fast">95<sub alias="pounds">#</sub> male, 75<sub alias="pounds">#</sub> female</prosody></s><s>Rest 1 Minutes</s><s>2 Minutes</s><s>15 Push Ups, 50 Double Unders, Max Reps <sub alias="overhead">OH</sub> Squats <prosody rate="fast">95<sub alias="pounds">#</sub> male, 75<sub alias="pounds">#</sub> female</prosody></s><s>Rest 1 Minutes</s><s>3 Minutes</s><s>15 Push Ups, 50 Double Unders, Max Reps <sub alias="overhead">OH</sub> Squats <prosody rate="fast">95<sub alias="pounds">#</sub> male, 75<sub alias="pounds">#</sub> female</prosody></s><s></s><s>Workout is complete when you complete 75 <sub alias="overhead">OH</sub> Squats, if you don't complete 75 <sub alias="overhead">OH</sub> Squats within the given intervals, your score is total number of squats</s></speak>
+'''.strip()
 
-GET_WOD_EVENT = {
+
+DATE_20170901 = datetime(2017, 9, 1, 12, tzinfo=env.UTC)
+
+
+def test_open_skill():
+    with patch_urlopen(EBCF_RESPONSE_WOD_20170901) as mock_urlopen, \
+             patch.object(env, 'now', return_value=DATE_20170901) as mock_now:
+        resp = lambda_handler(OPEN_SKILL, NonCallableMagicMock(name='context'))
+        assert mock_urlopen.called
+        assert mock_now.called
+        assert_valid_response(resp)
+        assert EBCF_RESPONSE_WOD_20170901_SSML == resp['response']['outputSpeech']['ssml']
+
+
+DEPRECATED_CODE_REQUEST = {
     "session": {
         "sessionId": "SessionId.dff1b708-2aeb-4d08-8fa8-aaf549836707",
         "application": {
@@ -38,85 +94,7 @@ GET_WOD_EVENT = {
     "version": "1.0"
 }
 
-YES_INTENT_EVENT = {
-  "session": {
-    "sessionId": "SessionId.d52a4dc9-90fe-4d83-92e0-569ef47832d8",
-    "application": {
-      "applicationId": "amzn1.ask.skill.d6f2f7c4-7689-410d-9c35-8f8baae37969"
-    },
-    "attributes": {"date": "2017-06-02"},
-    "user": {
-      "userId": "amzn1.ask.account.AGF7EUF4RNORLHSZDNU7KR7W75A2GRGQPT6OMHLBACZBLFKZTA2SPNW2UR527IFJRSPTPMMG5F2J64FH67DWLVUYNRDO5IOLQ2OSS22UJAMPG7YLDFDFSMMVQKWUIIIX5PI3RBDV4YGFZN6M5LR2GV52NQND5PJPVHVE3NAYGSGPLNNPDI6PYTKNAQMBJW2KLONN2Z7F77FUZPA"
-    },
-    "new": False
-  },
-  "request": {
-    "type": "IntentRequest",
-    "requestId": "EdwRequestId.6c073620-a9db-4c8e-be8d-ab8d819a525a",
-    "locale": "en-US",
-    "timestamp": "2017-06-06T00:38:36Z",
-    "intent": {
-      "name": "AMAZON.YesIntent",
-      "slots": {}
-    }
-  },
-  "version": "1.0"
-}
-
-EXPECTED = {
-  "version": "1.0",
-  "response": {
-    "outputSpeech": {
-      "type": "SSML",
-      "ssml": "<speak><p>Announcement: NO EVENING CLASSES TODAY</p><p>Strength Section:</p><s>Front Squat</s><s>4 sets of 4</s><s>Go up 5<sub alias=\"pounds\">#</sub> from last week</s><p>Conditioning:</p><s>15 Min AMRAP</s><s>30 Wall Balls 20<sub alias=\"pounds\">#</sub> male, 14<sub alias=\"pounds\">#</sub> female, 10<sub alias=\"feet\">'</sub> male, 9<sub alias=\"feet\">'</sub> female</s><s>30 Ring Dips</s><s>30 Pistols</s><s>30 <sub alias=\"dumbbell\">DB</sub> Clean and Jersks 50<sub alias=\"pounds\">#</sub> male, 35<sub alias=\"pounds\">#</sub> female</s></speak>"
-    },
-    "card": {
-      "content": "Strength:\nNO EVENING CLASSES TODAY\n\nFront Squat\n4x4\nGo up 5# from last week\nConditioning:\n15 Min AMRAP\n30 Wall Balls 20#/14#, 10'/9'\n30 Ring Dips\n30 Pistols\n30 DB Clean & Jersks 50#/35#\nImage: None",
-      "title": "Elliott Bay Crossfit - WOD for 2017-06-02",
-      "type": "Simple"
-    },
-    "reprompt": {
-      "outputSpeech": {
-        "type": "PlainText",
-        "text": "Ask me what is the wod for other days of the week, or what was the wod on a past day!"
-      }
-    },
-    "shouldEndSession": True
-  },
-  "sessionAttributes": {}
-}
-
-
-@pytest.fixture(scope='module', params=[GET_WOD_EVENT, YES_INTENT_EVENT])
-def lambda_response(request):
-    with patch.object(wods, 'urlopen', mock_open(read_data=EBCF_WEBSITE_JSON)):
-        return lambda_handler(request.param, NonCallableMagicMock())
-
-
-@pytest.mark.xfail(reason='currently none of these intents are supported')
-def test_response_has_version(lambda_response):
-    assert lambda_response['version'] == '1.0'
-
-
-@pytest.mark.xfail(reason='currently none of these intents are supported')
-def test_response_has_sessionAttributes(lambda_response):
-    assert lambda_response['sessionAttributes'] == {}
-
-
-@pytest.fixture(scope='module', params=[
-    lambda r: r['outputSpeech'],
-    pytest.mark.xfail(lambda r: r['reprompt']['outputSpeech'], reason='No reprompt on success')
-])
-def output_speech(lambda_response, request):
-    return request.param(lambda_response['response'])
-
-
-@pytest.mark.xfail(reason='currently none of these intents are supported')
-def test_response_output_speech(output_speech):
-    assert output_speech['type'] in ('SSML', 'PlainText')
-    if output_speech['type'] == 'SSML':
-        assert output_speech['ssml'].startswith('<speak>')
-        assert output_speech['ssml'].endswith('</speak>')
-    elif output_speech['type'] == 'PlainText':
-        assert output_speech['text'].strip()
-
+@pytest.mark.xfail(reason='we currently dont handle interaction model changes in code.')
+def test_interaction_model_breaking_change():
+    resp = lambda_handler(DEPRECATED_CODE_REQUEST, NonCallableMagicMock('context'))
+    assert_valid_response(resp)
